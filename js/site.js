0a24b1917e9f914cf6d5e5724de773697a76ea1f
KennyNtozakhe <mpentozakhe@gmail.com>
2024-03-24 12:02:02 +0200

    making things work

--- js/site.js
+++ js/site.js
@@ -1,4 +1,33 @@
-function HelloWorld(){
-    alert("Hello World");
+//Get the string from the page
+//controller function
+function getValue() {
+    document.querySelector('#alert').classList.add('invisible');
+    let userString = document.querySelector('#userString').value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
+
 }
 
+//Reverse the String
+//Logic function
+function reverseString(userString) {
+let revString =[];
+
+//reverse a string using a for loop
+for(let i = userString.length -1; i >= 0; i--){
+    revString += userString[i];
+}
+return revString;
+}
+//Display our reversed string tp the user.
+//View Function
+function displayString(revString) {
+   //write to the page
+document.querySelector('#msg').innerHTML = `Your string reversed is: <strong>${revString}</strong>`;
+
+//show the alert box
+document.querySelector('#alert').classList.remove('invisible');
+
+}
